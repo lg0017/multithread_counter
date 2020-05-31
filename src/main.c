0a8f29cb75983ba576efccb01e46efb8d2ffa995
lg0017 <luizmb16@gmail.com>
2020-05-31 20:25:47 -0300

    Corrigindo paralelismo

--- src/main.c
+++ src/main.c
@@ -8,9 +8,16 @@
 #include <time.h>
 #include <pthread.h>
 
+typedef struct {
+	int dado_entrada;
+	int id_thread;
+} thread_dados;
+
 /* Variaveis globais */
 pthread_mutex_t trava;
 int contador = 0;
+int libera[4];
+int k = 0;
 
 /* Funcao que verifica dado um parametro se o numero n e primo */
 int primo(int n){
@@ -33,18 +40,18 @@ int primo(int n){
 
 /* Funcao worker que verifica todos os numeros */
 void* worker(void *arg) {
-	int *a = (int*)(arg);
-    	int b = (*a);
+	
+	thread_dados *infos_worker = (thread_dados *)arg; 
 	int aux = 0;
 	
-    	pthread_mutex_lock(&trava);
+    pthread_mutex_lock(&trava);
 	
-		aux = primo(b);
-		if(aux){
-			contador++;
-		}
-		
-    	pthread_mutex_unlock(&trava);
+	aux = primo(infos_worker->dado_entrada);
+	if(aux){
+		contador++;
+	}
+	libera[infos_worker->id_thread] = 0;	
+    pthread_mutex_unlock(&trava);
         
  			
 	return NULL;
@@ -56,8 +63,17 @@ int main() {
 
 	/* Variaveis de auxilio geral */
 	int entrada[100];
-	int qtd = 0, i = 0, j = 0, k = 0, saida = 0, inteiro;
+	int qtd = 0, i = 0, j = 0, saida = 0, inteiro;
 	char caracter;
+	thread_dados *infos;
+
+	/* Preenchendo o vetor liberador de zeros */
+	/* Legenda: 0 - thread livre 1 - thread ocupada */
+	for(i = 0; i < 4; i++){
+		libera[i] = 0;
+	}
+
+	i = 0;
 
 	/* Tratamento da entrada */	
 	while (saida != -1){
@@ -79,17 +95,16 @@ int main() {
 
 		/* Numero maximo de threads ao mesmo tempo */
 		for(k = 0; k < 4; k++){
-			if(j < qtd){
-				pthread_create(&(threads[k]), NULL, worker, (&entrada[j]));
+			if(libera[k]== 0){
+				libera[k] = 1;
+				infos = (thread_dados*)malloc(sizeof(thread_dados));
+				infos ->dado_entrada = entrada[j];
+				infos ->id_thread = k;
+				pthread_create(&(threads[k]), NULL, worker, (void*) infos);
 				j++;
+				break;
 			}
 		}
-
-		/* Esperando threads terminarem */
-		for (int i = 0; i < 4; i++) {
-			pthread_join(threads[i], NULL);
-		}
-		
 	}
 
 	/* Verifica se as threads acabaram */
@@ -103,3 +118,4 @@ int main() {
 	return 0;
 	
 }
+
